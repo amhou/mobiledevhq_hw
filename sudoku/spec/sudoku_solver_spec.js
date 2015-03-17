da4f551bfa86afb43138647ac45e1f0cff4a4841
Andrew Hou <hou.andrew@gmail.com>
2015-03-17 00:41:19 -0700

    Can now fully solve sudoku puzzles, includes tests

--- sudoku/spec/sudoku_solver_spec.js
+++ sudoku/spec/sudoku_solver_spec.js
@@ -14,15 +14,15 @@ var parsedBoard;
 describe("#parseBoard()", function() {
   it("should parse a standard sudoku board into a 2D array", function() {
     parsedBoard = sudoku_solver.parseBoard(testBoard);
-    var expectedBoard = [["-","-",4,"-",5,3,"-","-",6],
-                         ["-","-",1,4,"-",2,"-","-","-"],
-                         [3,"-",6,8,"-","-","-","-","-"],
-                         ["-",9,3,"-","-","-",8,"-","-"],
-                         ["-",2,5,3,"-",1,6,7,"-"],
-                         ["-","-",8,"-","-","-",4,2,"-"],
-                         ["-","-","-","-","-",5,3,"-",7],
-                         ["-","-","-",7,"-",9,1,"-","-"],
-                         [5,"-","-",1,4,"-",9,"-","-"]];
+    var expectedBoard = [[0,0,4,0,5,3,0,0,6],
+                         [0,0,1,4,0,2,0,0,0],
+                         [3,0,6,8,0,0,0,0,0],
+                         [0,9,3,0,0,0,8,0,0],
+                         [0,2,5,3,0,1,6,7,0],
+                         [0,0,8,0,0,0,4,2,0],
+                         [0,0,0,0,0,5,3,0,7],
+                         [0,0,0,7,0,9,1,0,0],
+                         [5,0,0,1,4,0,9,0,0]];
 
     expect(parsedBoard.length).toEqual(9);
 
@@ -38,16 +38,59 @@ describe("#findEmptyCells()", function() {
   it("should find the empty cells of the given sudoku board", function() {
     var emptyCells = sudoku_solver.findEmptyCells(parsedBoard);
     var expectedEmptyCells = [[0,0],[0,1],[0,3],[0,6],[0,7],
-                             [1,0],[1,1],[1,4],[1,6],[1,7],[1,8],
-                             [2,1],[2,4],[2,5],[2,6],[2,7],[2,8],
-                             [3,0],[3,3],[3,4],[3,5],[3,7],[3,8],
-                             [4,0],[4,4],[4,8],
-                             [5,0],[5,1],[5,3],[5,4],[5,5],[5,8],
-                             [6,0],[6,1],[6,2],[6,3],[6,4],[6,7],
-                             [7,0],[7,1],[7,2],[7,4],[7,7],[7,8],
-                             [8,1],[8,2],[8,5],[8,7],[8,8]];
+                              [1,0],[1,1],[1,4],[1,6],[1,7],[1,8],
+                              [2,1],[2,4],[2,5],[2,6],[2,7],[2,8],
+                              [3,0],[3,3],[3,4],[3,5],[3,7],[3,8],
+                              [4,0],[4,4],[4,8],
+                              [5,0],[5,1],[5,3],[5,4],[5,5],[5,8],
+                              [6,0],[6,1],[6,2],[6,3],[6,4],[6,7],
+                              [7,0],[7,1],[7,2],[7,4],[7,7],[7,8],
+                              [8,1],[8,2],[8,5],[8,7],[8,8]];
 
     expect(emptyCells.length).toEqual(49);
     expect(emptyCells).toEqual(expectedEmptyCells);
+  });
+});
+
+describe("#checkRow()", function() {
+  it("should check that the given value does not exist within the given row", function() {
+    expect(sudoku_solver.checkRow(parsedBoard, 0, 1)).toBe(true);
+    expect(sudoku_solver.checkRow(parsedBoard, 0, 4)).toBe(false);
+  });
+});
+
+describe("#checkColumn()", function() {
+  it("should check that the given value does not exist within the given column", function() {
+    expect(sudoku_solver.checkColumn(parsedBoard, 0, 1)).toBe(true);
+    expect(sudoku_solver.checkColumn(parsedBoard, 0, 5)).toBe(false);
+  });
+});
+
+describe("#checkZone()", function() {
+  it("should check that the given value does not exist within the given cell's 3x3 zone", function() {
+    expect(sudoku_solver.checkZone(parsedBoard, [0,0], 2)).toBe(true);
+    expect(sudoku_solver.checkZone(parsedBoard, [0,0], 1)).toBe(false);
+  });
+});
+
+describe("#checkValue()", function() {
+  it("should check that the given value is a valid placement in the given cell", function() {
+    expect(sudoku_solver.checkValue(parsedBoard, [0,0], 2)).toBe(true);
+    expect(sudoku_solver.checkValue(parsedBoard, [1,0], 2)).toBe(false);
+  });
+});
+
+describe("#solveSudoku()", function() {
+  it("should solve a sudoku puzzle provided as a CSV", function() {
+    var expectedSolution = [[2,8,4,9,5,3,7,1,6],
+                            [9,7,1,4,6,2,5,3,8],
+                            [3,5,6,8,1,7,2,9,4],
+                            [6,9,3,2,7,4,8,5,1],
+                            [4,2,5,3,8,1,6,7,9],
+                            [7,1,8,5,9,6,4,2,3],
+                            [1,4,9,6,2,5,3,8,7],
+                            [8,6,2,7,3,9,1,4,5],
+                            [5,3,7,1,4,8,9,6,2]]
+    expect(sudoku_solver.solveSudoku(testBoard)).toEqual(expectedSolution);
   });
 });
--- sudoku/sudoku_solver.js
+++ sudoku/sudoku_solver.js
@@ -1,14 +1,49 @@
-var testBoard = "-,-,4,-,5,3,-,-,6\n" +
-                "-,-,1,4,-,2,-,-,-\n" +
-                "3,-,6,8,-,-,-,-,-\n" +
-                "-,9,3,-,-,-,8,-,-\n" +
-                "-,2,5,3,-,1,6,7,-\n" +
-                "-,-,8,-,-,-,4,2,-\n" +
-                "-,-,-,-,-,5,3,-,7\n" +
-                "-,-,-,7,-,9,1,-,-\n" +
-                "5,-,-,1,4,-,9,-,-";
+/*
+ * Solves a sudoku puzzle!
+ *
+ * Args: rawBoard = a string representation of a sudoku board, comma delimited.
+ * Returns: a 9x9 2D array solution
+ */
+var solveSudoku = function(rawBoard) {
+  const SUDOKU_MAX = 9;
+
+  var row, column, value, found;
+
+  var board = parseBoard(rawBoard);
+  var emptyCells = findEmptyCells(board);
+
+  for (i = 0;  i < emptyCells.length;) {
+    row = emptyCells[i][0];
+    column = emptyCells[i][1];
+
+    value = board[row][column] + 1;
+    found = false;
+
+    while(!found && value <= SUDOKU_MAX) {
+      if (checkValue(board, emptyCells[i], value)) {
+        found = true;
+        board[row][column] = value;
+        i++;
+      } else {
+        value++;
+      }
+    }
+
+    // If no value is found, reset cell to 0 and go backwards
+    if (!found) {
+      board[row][column] = 0;
+      i--;
+    }
+  }
+
+  return board;
+}
+
 /*
  * Parses a CSV sudoku board into a 2D array.
+ *
+ * Args: rawBoard - a string representation of a sudoku board, comma delimited.
+ * Returns: a 9x9 2D array
  */
 var parseBoard = function (rawBoard) {
   var parsedBoard = rawBoard.split("\n").map(function(row) {
@@ -16,7 +51,7 @@ var parseBoard = function (rawBoard) {
       if (value != "-") {
         return parseInt(value);
       } else {
-        return value;
+        return 0;
       }
     });
   });
@@ -26,12 +61,15 @@ var parseBoard = function (rawBoard) {
 
 /*
  * Finds the empty cells of a given sudoku board.
+ *
+ * Args: board - a 9x9 2D array
+ * Returns: the coordinates of empty cells
  */
 var findEmptyCells = function (board) {
   var emptyCells = [];
   for (row = 0; row < board.length; row++) {
     for (column = 0; column < board[row].length; column++) {
-      if (board[row][column] === "-") {
+      if (board[row][column] === 0) {
         emptyCells.push([row,column]);
       }
     }
@@ -40,6 +78,92 @@ var findEmptyCells = function (board) {
   return emptyCells;
 }
 
+/*
+ * Checks that the given value is a valid placement in the given cell.
+ *
+ * Args: board - a 9x9 2D array
+ *       cell - the coordinates for a cell (row x column)
+ *       value - an integer
+ * Returns: true if the value is not in the cell's zone, row, or column
+ */
+var checkValue = function(board, cell, value) {
+  if (checkRow(board, cell[0], value) &&
+      checkColumn(board, cell[1], value) &&
+      checkZone(board, cell, value)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/*
+ * Checks that the given value does not exist within the given row.
+ *
+ * Args: board - a 9x9 2D array
+ *       row - the row to be checked
+ *       value - an integer
+ * Returns: true if the value is not in the given row
+ */
+var checkRow = function(board, row, value) {
+  return board[row].indexOf(value) === -1;
+}
+
+/*
+ * Checks that the given value does not exist within the given column.
+ *
+ * Args: board - a 9x9 2D array
+ *       column - the column to be checked
+ *       value - an integer
+ * Returns: true if the value is not in the given column
+ */
+var checkColumn = function(board, column, value) {
+  for (row = 0; row < board.length; row++) {
+    if (board[row][column] === value) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/*
+ * Checks that the given value does not exist within the given cell's 3x3 zone.
+ *
+ * Args: board - a 9x9 2D array
+ *       cell - the coordinates for a cell (row x column)
+ *       value - an integer
+ * Returns: true if the value is not in the cell's zone
+ */
+var checkZone = function(board, cell, value) {
+  var zoneLeftEdge = 0;
+  var zoneTopEdge = 0;
+  var zoneWidth = 3;
+
+  // Find zone's left edge
+  while (cell[1] >= zoneLeftEdge + zoneWidth) {
+    zoneLeftEdge += zoneWidth;
+  }
+
+  // Find zone's top edge
+  while (cell[0] >= zoneTopEdge + zoneWidth) {
+    zoneTopEdge += zoneWidth;
+  }
+
+  // Check the found zone
+  for (row = zoneTopEdge; row < zoneTopEdge + zoneWidth; row++) {
+    for (column = zoneLeftEdge; column < zoneLeftEdge + zoneWidth; column++) {
+      if (board[row][column] === value) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
 // Function exports
+exports.solveSudoku = solveSudoku;
 exports.parseBoard = parseBoard;
 exports.findEmptyCells = findEmptyCells;
+exports.checkValue = checkValue;
+exports.checkRow = checkRow;
+exports.checkColumn = checkColumn;
+exports.checkZone = checkZone;
